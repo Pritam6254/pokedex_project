c87e4690c2e9043fc0b824f3a68138c35307b3aa
Pritam6345 <narayanpritam09@gmail.com>
2024-11-18 02:18:42 +0530

    fix: correct state update logic for navigation buttons in PokemonList

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -6,30 +6,41 @@ import './PokemonList.css'
 import Pokemon from "../Pokemon/Pokemon";
 function PokemonList() {
 
-    const [pokemonList,setPokemonList] = useState([]);
-    const [isLoading,setIsLoading] = useState(true)
+    // const [pokemonList,setPokemonList] = useState([]);
+    // const [isLoading,setIsLoading] = useState(true)
 
-    const [pokedexUrl,setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+    // const [pokedexUrl,setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon');
 
-    const [nextUrl, setNextUrl] = useState('');
-    const [prevUrl, setPrevUrl] = useState('');
+    // const [nextUrl, setNextUrl] = useState('');
+    // const [prevUrl, setPrevUrl] = useState('');
 
-    
+    const [pokemonListState,setPokemonListState] = useState({
+        pokemonList: [],
+        isLoading: true,
+        pokedexUrl: 'https://pokeapi.co/api/v2/pokemon',
+        nextUrl: '',
+        prevUrl: ''
+    })    
 
     async function downloadPokemons() {
-        setIsLoading(true)
-        const response = await axios.get(pokedexUrl);
+        setPokemonListState({...pokemonListState, isLoading:true})
+        const response = await axios.get(pokemonListState.pokedexUrl);
 
         const pokemonResults = response.data.results;
+
+        setPokemonListState((state) =>({
+            ...state, 
+            nextUrl: response.data.next, 
+            prevUrl:response.data.previous}));
 
         const pokemonResultPromis = pokemonResults.map((pokemon) => axios.get(pokemon.url));
         const pokemonData = await axios.all(pokemonResultPromis);
 
         console.log(response.data);
 
-        setNextUrl(response.data.next);
-        setPrevUrl(response.data.previous);
-        const pokeLIstRes = pokemonData.map((pokeData) => {
+        
+        
+        const pokeListRes = pokemonData.map((pokeData) => {
             const pokemon = pokeData.data;
             const pokemonId = pokemon.id;
             return {
@@ -40,28 +51,43 @@ function PokemonList() {
             }
             
         });
-        console.log(pokeLIstRes);
-        setPokemonList(pokeLIstRes);
-        setIsLoading(false)
+        console.log(pokeListRes);
+        setPokemonListState((state) =>({...state,pokemonList: pokeListRes, isLoading: false}));
     }
     useEffect(()=> {
         downloadPokemons();
-    },[pokedexUrl]);
+    },[pokemonListState.pokedexUrl]);
 
     
 
     return (
         <div className="pokemon-list-wrapper">
            <div className="pokemon-wrapper">
-            {(isLoading) ? 'Loading...' : 
-                pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id} />)
+            {(pokemonListState.isLoading) ? 'Loading...' : 
+                pokemonListState.pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id} />)
             }
             </div>
 
-            <div className="controls">
-                <button disabled={prevUrl == null} onClick={() => setPokedexUrl(prevUrl)}>Prev</button>
-                <button disabled={nextUrl == null} onClick={() => setPokedexUrl(nextUrl)}>Next</button>
-            </div>
+        <div className="controls">
+            <button
+            disabled={pokemonListState.prevUrl == null}
+            onClick={() => {
+                const urlToSet = pokemonListState.prevUrl;
+                setPokemonListState({ ...pokemonListState, pokedexUrl: urlToSet });
+            }}
+            >
+                Prev
+            </button>
+            <button
+                disabled={pokemonListState.nextUrl == null}
+                onClick={() => {
+                    const urlToSet = pokemonListState.nextUrl;
+                    setPokemonListState({ ...pokemonListState, pokedexUrl: urlToSet });
+            }}
+            >
+                Next
+            </button>
+        </div>
         </div>
     )
 
